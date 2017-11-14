2385b6dcd974d652b2b1c7d833ec165173c0e331
Manfred Linzner <git@linzner.net>
2017-11-14 10:14:32 +0100

    Added sample python file with issues

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+import json
+
+import uuid4
+
+
+
+
+foo = ['Maecenas sed diam eget risus varius blandit sit amet non magna.','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur est at lobortis.','Etiam porta sem malesuada ','magna mollis euismod.']
+
+
+print json.dumps(foo)
